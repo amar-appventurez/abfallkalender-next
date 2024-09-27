6b76be58ad285a6eadb5bd17ed52a60b25591644
amar.mishra <amar.mishra@appventurez.com>
2024-09-27 13:23:30 +0530

    coverted api route for session creation to post

--- src/app/api/session/route.js
+++ src/app/api/session/route.js
@@ -8,64 +8,64 @@ import { createSession } from "../../../session";
  */
 
 // // POST request handler
-// export async function POST(request) {
-//   try {
-//     const { userDetails } = await request.json(); // Parse request body
-//     const sessionToken = await createSession(userDetails); // Create session
+export async function POST(request) {
+  try {
+    const { userDetails } = await request.json(); // Parse request body
+    const sessionToken = await createSession({userDetails}); // Create session
 
-//     const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // Set expiration
+    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // Set expiration
  
-//     // Create a redirect response
-//     const response = NextResponse.redirect(`${process.env.NEXT_SERVER ?? "http://localhost/3000"}/home`); // Redirect to /home
+    // Create a redirect response
+    const response = NextResponse.redirect(`${process.env.NEXT_SERVER ?? "http://localhost/3000/"}home`); // Redirect to /home
 
-//     // Set the session cookie
-//     response.cookies.set({
-//       name: "session",
-//       value: sessionToken,
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === "production",
-//       expires: expiresAt,
-//       sameSite: "strict",
-//       path: "/",
-//     });
+    // Set the session cookie
+    response.cookies.set({
+      name: "session",
+      value: sessionToken,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      expires: expiresAt,
+      sameSite: "strict",
+      path: "/",
+    });
 
-//     return response; // Return the redirect response with the cookie
-//   } catch (error) {
-//     return NextResponse.json({ success: false, error: error.message }, { status: 500 });
-//   }
-// }
+    return response; // Return the redirect response with the cookie
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+  }
+}
 
 
 /** Handler for get route for testing through browser */
 
-export async function GET(request) {
-    // Extract query parameters (e.g., tokens or user info)
-    const { searchParams } = new URL(request.url);
-    const oauthToken = searchParams.get('token');
-    const userName = searchParams.get('userName');
-    const email= searchParams.get('email')
-    if (!oauthToken) {
-        return NextResponse.redirect('/error');  // Handle OAuth failure
-    }
+// export async function GET(request) {
+//     // Extract query parameters (e.g., tokens or user info)
+//     const { searchParams } = new URL(request.url);
+//     const oauthToken = searchParams.get('token');
+//     const userName = searchParams.get('userName');
+//     const email= searchParams.get('email')
+//     if (!oauthToken) {
+//         return NextResponse.redirect('/error');  // Handle OAuth failure
+//     }
 
-    // Use the token and user data to create a session
-    const userDetails = {
-        token: oauthToken,
-        userName: userName,
-        email
-    };
+//     // Use the token and user data to create a session
+//     const userDetails = {
+//         token: oauthToken,
+//         userName: userName,
+//         email
+//     };
 
-    // Create the session with the token
-    const sessionToken = await createSession(userDetails);
+//     // Create the session with the token
+//     const sessionToken = await createSession(userDetails);
 
-    // Set the session cookie in the response
-    const response = NextResponse.redirect(`${process.env.NEXT_SERVER ?? "http://localhost/3000"}/home`);  // Redirect to homepage after successful login
-    response.cookies.set('session', sessionToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        path: '/',
-    });
+//     // Set the session cookie in the response
+//     const response = NextResponse.redirect(`${process.env.NEXT_SERVER ?? "http://localhost/3000/"}home`);  // Redirect to homepage after successful login
+//     response.cookies.set('session', sessionToken, {
+//         httpOnly: true,
+//         secure: process.env.NODE_ENV === 'production',
+//         sameSite: 'strict',
+//         path: '/',
+//     });
 
-    return response;
-}
+//     return response;
+// }
--- src/constants/common.js
+++ src/constants/common.js
@@ -1,15 +1,15 @@
 import CryptoJS from 'crypto-js';
 
-const TOKEN_ENCRYPTION_KEY= ``;
+
 export const encryptToken = (data) => {
     data = data.toString();
-    const encrypt = CryptoJS.AES.encrypt(data, TOKEN_ENCRYPTION_KEY);
+    const encrypt = CryptoJS.AES.encrypt(data, process.env.TOKEN_ENCRYPTION_KEY);
     return encrypt.toString();
   };
   
   export const decryptToken = (data) => {
     // const test="U2FsdGVkX18w7PVDIIRCdYsZuokCQjWrH3ZIv7ZiMpmYzLVQMrNeOiNRsGfqEHVfQARXnTnA4H9b9M3R9sOzl5FZZxp1I4o4N5owe62mxbAu/bUkdSoo5QijEx5iRx7pDYJ/0kn39Ump2rUpVPMDRyNmgrY2OMMU5VoYzPV2lVunDSBSTWwCZf/g5ySa/v220XfZ6/A7XgyleomsWxUj/O9I2a+HQEAEu8jWsMcH1rBYe17tXvWW9jUL5h98mPQA"
-    const decrypt = CryptoJS.AES.decrypt(data, TOKEN_ENCRYPTION_KEY);
+    const decrypt = CryptoJS.AES.decrypt(data, process.env.TOKEN_ENCRYPTION_KEY);
     //  console.log( decrypt.toString(CryptoJS.enc.Utf8) === "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByYWpqd2FsLnZlcm1hQGtvYmlsLmNvbSIsImlhdCI6MTcyNTAxNjkxNCwiZXhwIjoxNzMzNjU2OTE0fQ.vhAqh0GvZs7jxsQbYEjyAmwvowE2QgrW2qKYghCg9RY" )
     return decrypt.toString(CryptoJS.enc.Utf8);
     // return " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByYWpqd2FsLnZlcm1hQGtvYmlsLmNvbSIsImlhdCI6MTcyNTAxNjkxNCwiZXhwIjoxNzMzNjU2OTE0fQ.vhAqh0GvZs7jxsQbYEjyAmwvowE2QgrW2qKYghCg9RY"
