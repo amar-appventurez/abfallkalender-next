361fa993ce0d4fa1822ba6e406d1117e573e136f
amar.mishra <amar.mishra@appventurez.com>
2024-09-30 19:45:46 +0530

    updated oauth  api path

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@
 import { Endpoints } from "../constants/Endpoint";
 import { redirect } from "next/navigation";
 export default async function Home() {
-  redirect(`${Endpoints.baseUrl}/login`);
+  redirect(`${Endpoints.baseUrl}/auth/login`);
 }
