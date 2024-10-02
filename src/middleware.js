b49855d697fe65a2bc34c80588a7fff441d246af
amar.mishra <amar.mishra@appventurez.com>
2024-10-03 02:07:26 +0530

    console log session in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -11,6 +11,7 @@ export async function middleware(request) {
         return NextResponse.next(); // Allow the request to proceed
     }
     const session = request.cookies.get("session");
+    console.log("Session data found in middleware", session)
     if (!session) {
         console.log("Session not found, redirecting to auth");
         // return NextResponse.redirect(`${process.env.NEXT_SERVER ?? 'http://localhost:3000/'}`); 
