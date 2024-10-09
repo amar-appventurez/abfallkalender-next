398c94aec2250fb876a2df5eaa922b0310bb18aa
amar.mishra <amar.mishra@appventurez.com>
2024-10-09 10:30:25 +0530

    make a redirect to external server

--- src/app/api/clear-session/route.js
+++ src/app/api/clear-session/route.js
@@ -1,13 +1,21 @@
 import { NextResponse } from "next/server";
 import { deleteSession } from "../../../session";
-import { Endpoints } from "../../../constants/Endpoint";
 
 
 export async function GET(request) {
         // Clear the session cookie
-        console.log("Clearing session and redirecting...");
-        await deleteSession();
+        // await deleteSession();
         // Perform redirection (server-side)
-        return NextResponse.redirect(`${Endpoints.baseUrl}/auth/login`);
+
+        const response= NextResponse.redirect(`${process.env.NEXT_SERVER ?? 'http://localhost:3000/'}`);
+
+        console.log("Clearing session cookies explicitly")
+        response.cookies.set('session', '', {
+            expires: new Date(0), // Set to a date in the past to delete the cookie
+            path: '/',            // Make sure the path matches where the cookie was set
+        });
+    
+        return response;
+        
     
 }
