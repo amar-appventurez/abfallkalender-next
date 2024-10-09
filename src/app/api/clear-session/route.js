9b1f872d7f45bc79bc72785586a9251c1ec22cd0
amar.mishra <amar.mishra@appventurez.com>
2024-10-09 23:51:23 +0530

    explicity set cookie expiration time as current

--- src/app/api/clear-session/route.js
+++ src/app/api/clear-session/route.js
@@ -7,14 +7,14 @@ export async function GET(request) {
         // Clear the session cookie
         // deleteSession();
         // Perform redirection (server-side)
-
-        const response= NextResponse.redirect(`${process.env.NEXT_SERVER ?? 'http://localhost:3000/'}home`);
+        const response= NextResponse.redirect(new URL('/home', request.url))
+        // const response= NextResponse.redirect(`${process.env.NEXT_SERVER ?? 'http://localhost:3000/'}home`);
         cookies().delete('session')
         console.log("Clearing session cookies explicitly")
         // response.cookies.delete('session');
-        // response.cookies.set('session', '', {
-        //     expires: new Date(0), // Set to a date in the past to delete the cookie
-        //     path: '/',   
-        // });
+        response.cookies.set('session', '', {
+            expires: new Date(0), // Set to a date in the past to delete the cookie
+            path: '/',   
+        });
         return response;   
 }
