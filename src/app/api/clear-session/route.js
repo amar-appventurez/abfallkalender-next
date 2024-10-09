ec958431bb35a00ba0c2c11bc98fda602f110e5b
amar.mishra <amar.mishra@appventurez.com>
2024-10-09 09:28:26 +0530

    deployment:bug fix

--- src/app/api/clear-session/route.js
+++ src/app/api/clear-session/route.js
@@ -1,12 +1,11 @@
 import { NextResponse } from "next/server";
 import { deleteSession } from "../../../session";
-import { decryptToken, isValidUTF8 } from "../../../constants/common";
 
 
 export async function GET(request) {
         // Clear the session cookie
         await deleteSession();
         // Perform redirection (server-side)
-        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER ?? 'http://localhost:3000/'}`, 303);
+        return NextResponse.redirect(`${process.env.NEXT_SERVER ?? 'http://localhost:3000/'}`);
     
 }
