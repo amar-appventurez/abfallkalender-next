39c017b854315620157097bc6ea353bd9ebe21ab
amar.mishra <amar.mishra@appventurez.com>
2024-10-09 10:10:19 +0530

    removed middleware and skipped session check at root route

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 
 import { Endpoints } from "../constants/Endpoint";
 import { redirect } from "next/navigation";
-import {getUserSession} from '../session';
+// import {getUserSession} from '../session';
 export default async function Root() {
-  const session= await getUserSession();
-  if(!session){
-    console.log("Redirecting to oauth server");
+  // const session= await getUserSession();
+  console.log("Redirecting to oauth server");
     redirect(`${Endpoints.baseUrl}/auth/login`);
+  // if(!session){
+    
    
-  }
-  // redirect(`${process.env.NEXT_SERVER}home`);
-  redirect('/home')
+  // }
+  // // redirect(`${process.env.NEXT_SERVER}home`);
+  // redirect('/home')
 }
