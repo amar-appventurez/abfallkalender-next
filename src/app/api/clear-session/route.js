8fa545033d9f7c4d6e3b500fb2aa2a98e7a6ad9c
amar.mishra <amar.mishra@appventurez.com>
2024-10-09 23:11:58 +0530

    bug fix

--- src/app/api/clear-session/route.js
+++ src/app/api/clear-session/route.js
@@ -9,9 +9,9 @@ export async function GET(request) {
         // Perform redirection (server-side)
 
         const response= NextResponse.redirect(`${process.env.NEXT_SERVER ?? 'http://localhost:3000/'}`);
-        // cookies().delete('session')
+        cookies().delete('session')
         console.log("Clearing session cookies explicitly")
-        response.cookies.delete('session');
+        // response.cookies.delete('session');
         // response.cookies.set('session', '', {
         //     expires: new Date(0), // Set to a date in the past to delete the cookie
         //     path: '/',   
