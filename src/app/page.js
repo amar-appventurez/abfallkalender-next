91a34ab0dc064424a4fddbfcf304da769b0ac97d
amar.mishra <amar.mishra@appventurez.com>
2024-10-10 10:48:18 +0530

    add check for session on root page

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,12 @@ import { Endpoints } from "../constants/Endpoint";
 import { redirect } from "next/navigation";
 // import {getUserSession} from '../session';
 export default async function Root() {
-  // const session= await getUserSession();
+  const session= await getUserSession();
   console.log("Redirecting to oauth server");
-    redirect(`${Endpoints.baseUrl}/auth/login`);
-  // if(!session){
     
-   
-  // }
+  if(!session){  
+    redirect(`${Endpoints.baseUrl}/auth/login`);
+  }
   // // redirect(`${process.env.NEXT_SERVER}home`);
-  // redirect('/home')
+  redirect('/home')
 }
