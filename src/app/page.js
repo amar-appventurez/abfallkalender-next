e24c13df0afc8ff86c83a1e317bdaa6e084af193
amar.mishra <amar.mishra@appventurez.com>
2024-10-15 17:48:08 +0530

    console log on root path session value

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import {getUserSession} from '../session';
 export default async function Root() {
   const session= await getUserSession();
+  console.log("Session found in root path", session)
   console.log("Redirecting to oauth server");
  
   if(!session){  
