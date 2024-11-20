5c1fe11e3ffcaa9445d150b7936b2697b42b409a
amar.mishra <amar.mishra@appventurez.com>
2024-11-20 10:55:21 +0530

    added login mechanism with super app

--- src/app/page.js
+++ src/app/page.js
@@ -4,21 +4,21 @@ import { redirect } from "next/navigation";
 import {decrypt, getUserSession} from '../session';
 import { cookies } from "next/headers";
 export default async function Root() {
-  // const cookieStore= cookies();
-  // let isTokenSet=false;
-  // try{
-  //   const {userDetails:{token}} =await decrypt(cookieStore.get('session')?.value);
-  //   if(token) isTokenSet=true
-  // }
-  // catch{
-  //   isTokenSet=false;
-  // }
+  const cookieStore= cookies();
+  let isTokenSet=false;
+  try{
+    const {userDetails:{token}} =await decrypt(cookieStore.get('session')?.value);
+    if(token) isTokenSet=true
+  }
+  catch{
+    isTokenSet=false;
+  }
   
  
-  // if(!isTokenSet){  
-  //   console.log("Redirecting to oauth server");
-  //   redirect(`${Endpoints.baseUrl}/auth/login`);
-  // }
+  if(!isTokenSet){  
+    console.log("Redirecting to oauth server");
+    redirect(`${Endpoints.baseUrl}/auth/login`);
+  }
     //redirect OIDC flow for address
     //redirect OIDC flow for consent page
    const consentGiven=true;
