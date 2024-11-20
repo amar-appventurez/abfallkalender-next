b8a14811a9c74d225c854fce05d53be5d930bef4
amar.mishra <amar.mishra@appventurez.com>
2024-11-20 10:57:14 +0530

    changed backend service endpoint

--- src/constants/Endpoint.js
+++ src/constants/Endpoint.js
@@ -1,4 +1,4 @@
 export const Endpoints={
-    baseUrl:`https://cityminiapps.kobil.com/app2/v1`,
+    baseUrl:`https://cityminiapps.kobil.com/app5/v1`,
     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByYWpqd2FsLnZlcm1hQGtvYmlsLmNvbSIsImlhdCI6MTcyNTAxNjkxNCwiZXhwIjoxNzMzNjU2OTE0fQ.vhAqh0GvZs7jxsQbYEjyAmwvowE2QgrW2qKYghCg9RY'
 }
